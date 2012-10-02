30de9a390ff79717c1664545b98256cbf325d92d
uclan-mobileapps <iphoneapps@uclan.ac.uk>
2012-10-02 10:24:00 +0100

    Licence Update

--- CourseExplorer/LISCourseViewController.h
+++ CourseExplorer/LISCourseViewController.h
@@ -1,10 +1,20 @@
 //
 //  LISCourseViewController.h
-//  UKCourses
-//
-//  Created by Criss Myers on 20/06/2012.
-//  Copyright (c) 2012 UCLan. All rights reserved.
-//
+/*
+Copyright 2012 UCLan (University of Central Lancashire)
+
+Licenced under the BSD 2-Clause Licence.
+You may not use this file except in compliance with the License.
+You may obtain a copy of the License at:
+
+       http://opensource.org/licenses/bsd-license.php
+
+Unless required by applicable law or agreed to in writing, software
+distributed under the License is distributed on an "AS IS" BASIS,
+WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+See the License for the specific language governing permissions and
+limitations under the License.
+*/
 
 #import <UIKit/UIKit.h>
 
